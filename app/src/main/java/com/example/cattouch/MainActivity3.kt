03629bbe86137b87d74682d561c4d0fa82fc403e
0zzam0 <78575152+0zzam0@users.noreply.github.com>
2021-12-08 03:58:42 +0900

    2

--- app/src/main/java/com/example/cattouch/MainActivity3.kt
+++ app/src/main/java/com/example/cattouch/MainActivity3.kt
@@ -2,7 +2,9 @@ package com.example.cattouch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.example.cattouch.databinding.ActivityMain3Binding
 
 class MainActivity3 : AppCompatActivity() {
@@ -12,13 +14,35 @@ class MainActivity3 : AppCompatActivity() {
     var country : String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //var data = listOf("South Korea", "Hong Kong", "Thailand", "Japan","United States", "Sweden")
         val binding = ActivityMain3Binding.inflate(layoutInflater)
+        //배열 가져오기
+        var sData = resources.getStringArray(R.array.country)
+        //어댑터로 연결
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sData)
+        binding.spinnerCountry.adapter = adapter
 
-        fun setupSpinnerHandler(){
-            binding.spinnerCountry.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-
+        //세팅
+        binding.spinnerCountry.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                TODO("Not yet implemented")
+                country = binding.spinnerCountry.getSelectedItem().toString()
             }
-    }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+        //텍스트
+        binding.Address.setText("Address")
+        binding.Name.setText("Name")
+        binding.Password.setText("Password")
+
 
     }
 }
