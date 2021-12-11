8b91c97f6361ef5009d8442c11f7f09c627d9d0c
0zzam0 <78575152+0zzam0@users.noreply.github.com>
2021-12-12 04:29:10 +0900

    join

--- app/src/main/java/com/example/cattouch/MainActivity.kt
+++ app/src/main/java/com/example/cattouch/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cattouch.databinding.ActivityMainBinding
+import com.example.cattouch.databinding.ActivityMain2Binding
+import com.example.cattouch.databinding.ActivityMain3Binding
 
 
 var count :Int =0
@@ -19,13 +21,16 @@ class MainActivity : AppCompatActivity() {
         //초기화 위해서 build.gradle -> android에 viewBinding true 추가하기
         val binding = ActivityMainBinding.inflate(layoutInflater)
         val intent = Intent(this, MainActivity2::class.java)
+        val intent2 = Intent(this, MainActivity3::class.java)
         //레이아웃 표시
         setContentView(binding.root);
 
         binding.startBtn.setOnClickListener {
             startActivity(intent)
         }
-
+        binding.imageButton.setOnClickListener{
+            startActivity(intent2)
+        }
 
 
             /*//쓰레드
