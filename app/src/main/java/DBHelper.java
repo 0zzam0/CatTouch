d99f6cec2c8424f71242cfa38728875b3eef93d1
이상락 <64376220+PangRAK@users.noreply.github.com>
2021-12-12 19:14:00 +0900

    a

---
+++ app/src/main/java/DBHelper.java
@@ -0,0 +1,21 @@
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //String sql = "CREATE TABLE
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
